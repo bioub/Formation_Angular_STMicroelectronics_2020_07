d80eae421360da34f36b29a3f1dc80d9d644fd56
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-07-07 10:09:52 +0200

    Jour 1.1

---
+++ rappels-dom-js/02-object.js
@@ -0,0 +1,8 @@
+// object literal (créé un objet)
+const coords = {
+  x: 1,
+  y: 2,
+};
+
+console.log(coords.x); // 1
+console.log(coords.y); // 2
---
+++ rappels-dom-js/03-egalite.js
@@ -0,0 +1,2 @@
+console.log(12 == '12'); // true, egalite (conversion)
+console.log(12 === '12'); // false, egalite stricte (pas de conversion)
